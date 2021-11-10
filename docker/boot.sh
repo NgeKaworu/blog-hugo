22141be6ac98f946b19bfc1b7b89d3be1c7fc48f
fuRan <NgeKaworu@gmail.com>
2021-11-10 11:50:46 +0800

    sh: boot

--- docker/boot.sh
+++ docker/boot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-docker pull ngekaworu/blog-hugo;
-docker compose -f ./docker-compose.yml --env-file ~/.env -p blog-hugo up -d;
+docker compose -f ./docker-compose.yml -p blog-hugo up -d;
